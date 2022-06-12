ccef38d589969cacc6a573e3545d25b546591b4a
Andrii Reva <rewa.andrei2010@gmail.com>
2022-06-12 22:36:13 +0300

    observer: add preferences UI logic
    - added preferences frontend logic.

---
+++ src/main/resources/static/js/preferences.js
@@ -0,0 +1,22 @@
+let indexPreferencesPage = document.location.href;
+
+document.addEventListener('DOMContentLoaded', function () {
+    let resources = document.querySelectorAll(".resource-js");
+    for (let i = 0; i < resources.length; i++) {
+        const resource = resources[i];
+        let resourceId = resource.children[0].textContent;
+        addEvent(resource, "click", function () {
+            // addResource
+            removeResource(resourceId);
+        });
+    }
+
+    function removeResource(id) {
+        if (confirm('Are you sure you want to remove resource ' + id + " ?")) {
+            fetch(indexPreferencesPage + "/resources/" + id, {
+                method: "DELETE",
+            })
+            document.location.href = indexPreferencesPage;
+        }
+    }
+});
