5ce5f2831e33add5959130e541d0ce46f3d2f39a
Andrii Reva <rewa.andrei2010@gmail.com>
2022-06-13 23:01:30 +0300

    observer: fix remove resource record logic in the preferences page
    - fixed remove logic, namely: added event to 'remove' button and not to all record;
    - added js docs.

--- src/main/resources/static/js/preferences.js
+++ src/main/resources/static/js/preferences.js
@@ -3,14 +3,37 @@ let indexPreferencesPage = getCurrentBrowserUrl();
 document.addEventListener('DOMContentLoaded', function () {
     let resources = document.querySelectorAll(".resource-js");
     for (let i = 0; i < resources.length; i++) {
-        const resource = resources[i];
+        let resource = resources[i];
         let resourceId = resource.children[0].textContent;
-        addEvent(resource, "click", function () {
-            // addResource
-            removeResource(resourceId);
-        });
+
+        addClickEventToRemoveButton(resource, resourceId);
     }
 
+    /**
+     * Add click event to Remove button on each resource record.
+     *
+     * @param resource 'resource-js' element.
+     * @param resourceId resource id from 'resource-id-js'.
+     */
+    function addClickEventToRemoveButton(resource, resourceId) {
+        let partOfResourceRecords = resource.childNodes;
+        for (let i = 0; i < partOfResourceRecords.length; i++) {
+            if (partOfResourceRecords[i].className) {
+                if (partOfResourceRecords[i].className === "resource-remove-js") {
+                    addEvent(resource.childNodes[i], "click", function () {
+                        removeResource(resourceId);
+                    });
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * Remove resource by resource id
+     *
+     * @param id resource id.
+     */
     function removeResource(id) {
         if (confirm('Are you sure you want to remove resource ' + id + " ?")) {
             fetch(indexPreferencesPage + "resources/" + id, {
