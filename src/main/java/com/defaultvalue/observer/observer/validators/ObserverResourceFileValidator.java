5b07374208b2c14be5608fa24c7bc7a5a558d15c
Andrii Reva <rewa.andrei2010@gmail.com>
2022-06-26 17:23:57 +0300

    observer: add java docs to ObserverResourceFileValidator
    - added java docs to ObserverResourceFileValidator

--- src/main/java/com/defaultvalue/observer/observer/validators/ObserverResourceFileValidator.java
+++ src/main/java/com/defaultvalue/observer/observer/validators/ObserverResourceFileValidator.java
@@ -8,24 +8,59 @@ import java.util.regex.Pattern;
 @Component
 public class ObserverResourceFileValidator {
 
-    public boolean isResourceEmpty(String line) {
-        return line.isEmpty();
-    }
-
+    /**
+     * Check if resource starts with comment character then skip this resource line in resource file.
+     *
+     * @param line             passed resource as String.
+     * @param commentCharacter passed comment character as String.
+     * @return boolean flag.
+     */
     public boolean isResourceStartsWithCommentChar(String line, String commentCharacter) {
-        return line.startsWith(commentCharacter);
+        return !isResourceEmpty(line) && line.startsWith(commentCharacter);
     }
 
+    /**
+     * Check is resource contains two or more comment characters in resource line.
+     * If resource contains two or more comment characters then skip this resource line in resource file.
+     *
+     * @param line             passed resource as String.
+     * @param commentCharacter passed comment character as String.
+     * @return boolean flag.
+     */
     public boolean isResourceContainsMoreThanOneCommentChar(String line, String commentCharacter) {
-        return calculateMatchCommentChar(line, commentCharacter) > 1;
+        return !isResourceEmpty(line) && calculateMatchCommentChar(line, commentCharacter) > 1;
     }
 
-    public boolean isResourceContainsSeparateLine(String line, String separateCharacter) {
-        return line.contains(separateCharacter);
-    }
-
+    /**
+     * Calculate count comment characters in one String.
+     *
+     * @param line             passed resource as String.
+     * @param commentCharacter passed comment character as String.
+     * @return boolean flag.
+     */
     int calculateMatchCommentChar(String line, String commentCharacter) {
         Matcher matcher = Pattern.compile(commentCharacter).matcher(line);
         return (int) matcher.results().count();
+    }
+
+    /**
+     * Check is resource contains separate character in resource line.
+     *
+     * @param line              passed resource as String.
+     * @param separateCharacter passed separate character as String.
+     * @return boolean flag.
+     */
+    public boolean isResourceContainsSeparateLine(String line, String separateCharacter) {
+        return !isResourceEmpty(line) && line.contains(separateCharacter);
+    }
+
+    /**
+     * Check is resource as String is null or is blank.
+     *
+     * @param line passed resource as String.
+     * @return boolean flag.
+     */
+    public boolean isResourceEmpty(String line) {
+        return line == null || line.isBlank();
     }
 }
