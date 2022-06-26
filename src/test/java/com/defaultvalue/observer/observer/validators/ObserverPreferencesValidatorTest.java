42a8d8c8a6a17f2b0825e6504313d04f4c1fcd9a
Andrii Reva <rewa.andrei2010@gmail.com>
2022-06-26 16:49:26 +0300

    observer: add unit tests on ObserverPreferencesValidator#isPathValid
    - added unit test coverage on ObserverPreferencesValidator#isPathValid

---
+++ src/test/java/com/defaultvalue/observer/observer/validators/ObserverPreferencesValidatorTest.java
@@ -0,0 +1,28 @@
+package com.defaultvalue.observer.observer.validators;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ObserverPreferencesValidatorTest {
+
+    private ObserverPreferencesValidator validator = new ObserverPreferencesValidator();
+
+    @Test
+    public void test_isPathValid() {
+        assertAll(
+                () -> assertTrue(validator.isPathValid("https://www.example.com")),
+                () -> assertTrue(validator.isPathValid("http://www.example.com")),
+                () -> assertTrue(validator.isPathValid("https://example.com")),
+                () -> assertTrue(validator.isPathValid("http://example.com")),
+                () -> assertFalse(validator.isPathValid("https://example")),
+                () -> assertFalse(validator.isPathValid("http://example")),
+                () -> assertFalse(validator.isPathValid("https://.")),
+                () -> assertFalse(validator.isPathValid("http://.")),
+                () -> assertFalse(validator.isPathValid("https://")),
+                () -> assertFalse(validator.isPathValid("http://"))
+        );
+    }
+}
