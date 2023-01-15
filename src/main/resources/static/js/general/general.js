4da91aa813d469238a0cb1a5c09abd02449f1d41
Andrii Reva <rewa.andrei2010@gmail.com>
2023-01-15 19:39:07 +0200

    observer: add change logo during back-forward navigation
    - added change logo during back-forward navigation.

--- src/main/resources/static/js/general/general.js
+++ src/main/resources/static/js/general/general.js
@@ -1,9 +1,22 @@
 
 window.addEventListener('beforeunload', function () {
-    changeLogo();
+    loadingPageLogo();
 });
 
-function changeLogo() {
-    document.querySelector(".logo2").style.display = "initial";
+window.addEventListener("pageshow", function (event) {
+    let historyTraversal = event.persisted ||
+        (window.performance && window.performance.getEntriesByType('navigation').map(nav => nav.type).includes('back_forward'));
+    if (historyTraversal) {
+        finishedPageLoad();
+    }
+});
+
+function loadingPageLogo() {
     document.querySelector(".logo").style.display = "none";
+    document.querySelector(".logo2").style.display = "initial";
+}
+
+function finishedPageLoad() {
+    document.querySelector(".logo").style.display = "initial";
+    document.querySelector(".logo2").style.display = "none";
 }
