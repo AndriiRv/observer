0c5c5360f128a8f26b3d97040d7d01fd3f011e50
Andrii Reva <rewa.andrei2010@gmail.com>
2023-08-19 19:52:26 +0300

    observer: refactor Observer Notification API to use on other pages
    - moved NotificationQueue init class to notification.js;
    - added creating only one NotificationQueue object on page.

--- src/main/resources/static/js/notification/notification.js
+++ src/main/resources/static/js/notification/notification.js
@@ -1,7 +1,138 @@
 
-var notificationQueueObj = new NotificationQueue();
+let notificationQueueObj;
+
+class NotificationQueue {
+
+    constructor() {
+        this._notifications = [];
+
+        this._topLeftNotificationQueueDivElement = document.createElement("div");
+        this._topLeftNotificationQueueDivElement.className = "notification-queue-js notification-queue-top-left-js";
+
+        this._topRightNotificationQueueDivElement = document.createElement("div");
+        this._topRightNotificationQueueDivElement.className = "notification-queue-js notification-queue-top-right-js";
+
+        this._bottomLeftNotificationQueueDivElement = document.createElement("div");
+        this._bottomLeftNotificationQueueDivElement.className = "notification-queue-js notification-queue-bottom-left-js";
+
+        this._bottomRightNotificationQueueDivElement = document.createElement("div");
+        this._bottomRightNotificationQueueDivElement.className = "notification-queue-js notification-queue-bottom-right-js";
+
+        document.querySelector(".root").append(
+            this._topLeftNotificationQueueDivElement,
+            this._topRightNotificationQueueDivElement,
+            this._bottomLeftNotificationQueueDivElement,
+            this._bottomRightNotificationQueueDivElement
+        );
+    }
+
+    get notifications() {
+        return this._notifications;
+    }
+
+    get topLeftNotificationQueueDivElement() {
+        return this._topLeftNotificationQueueDivElement;
+    }
+
+    get topRightNotificationQueueDivElement() {
+        return this._topRightNotificationQueueDivElement;
+    }
+
+    get bottomLeftNotificationQueueDivElement() {
+        return this._bottomLeftNotificationQueueDivElement;
+    }
+
+    get bottomRightNotificationQueueDivElement() {
+        return this._bottomRightNotificationQueueDivElement;
+    }
+
+    addToQueue(notificationObject) {
+        this._notifications.push(notificationObject);
+        this.renderNotificationQueues();
+
+        const currentNotificationQueueObj = this;
+
+        addEvent(notificationObject.domElement, "click", function () {
+            currentNotificationQueueObj.removeFromQueue(notificationObject);
+            notificationObject.domElement.remove();
+        });
+
+        setTimeout(function () {
+            currentNotificationQueueObj.removeFromQueue(notificationObject);
+            notificationObject.domElement.remove();
+            currentNotificationQueueObj.renderNotificationQueues();
+        }, notificationObject.timeMs);
+    }
+
+    removeFromQueue(notificationObject) {
+        for (let i = 0; i < this._notifications.length; i++) {
+            if (this._notifications[i] === notificationObject) {
+                this._notifications.splice(i, 1);
+            }
+        }
+    }
+
+    renderNotificationQueues() {
+        let topLeftNotifications = [];
+        let topRightNotifications = [];
+        let bottomLeftNotifications = [];
+        let bottomRightNotifications = [];
+
+        for (let notification of this.notifications) {
+            if (notification.location === NotificationLocation.NOTIFICATION_LOCATION.TOP_LEFT) {
+                topLeftNotifications.push(notification);
+                this.removeUnusedFromQueue(topLeftNotifications);
+            } else if (notification.location === NotificationLocation.NOTIFICATION_LOCATION.TOP_RIGHT) {
+                topRightNotifications.push(notification);
+                this.removeUnusedFromQueue(topRightNotifications);
+            } else if (notification.location === NotificationLocation.NOTIFICATION_LOCATION.BOTTOM_LEFT) {
+                bottomLeftNotifications.push(notification);
+                this.removeUnusedFromQueue(bottomLeftNotifications);
+            } else {
+                bottomRightNotifications.push(notification);
+                this.removeUnusedFromQueue(bottomRightNotifications);
+            }
+        }
+
+        for (let topLeftNotification of topLeftNotifications) {
+            this.topLeftNotificationQueueDivElement.append(topLeftNotification.domElement);
+        }
+
+        for (let topRightNotification of topRightNotifications) {
+            this.topRightNotificationQueueDivElement.append(topRightNotification.domElement);
+        }
+
+        for (let bottomLeftNotification of bottomLeftNotifications) {
+            this.bottomLeftNotificationQueueDivElement.append(bottomLeftNotification.domElement);
+        }
+
+        for (let bottomRightNotification of bottomRightNotifications) {
+            this.bottomRightNotificationQueueDivElement.append(bottomRightNotification.domElement);
+        }
+    }
+
+    /**
+     * Remove notifications from start indexes when count of notifications more than 4.
+     *
+     * @param notifications
+     */
+    removeUnusedFromQueue(notifications) {
+        if (notifications.length > 4) {
+            let removedElement = notifications.shift();
+            if (removedElement) {
+                removedElement.domElement.remove();
+            }
+        }
+    }
+}
 
 class Notification {
+
+    static {
+        if (!notificationQueueObj) {
+            notificationQueueObj = new NotificationQueue();
+        }
+    }
 
     constructor(headerText, description, location, type, timeMs) {
         this._headerText = headerText;
@@ -67,7 +198,21 @@ class Notification {
         this.buildNotification();
     }
 
+    buildErrorNotificationOnBottomRight() {
+        this.location = NotificationLocation.NOTIFICATION_LOCATION.BOTTOM_LEFT;
+        this.type = NotificationType.NOTIFICATION_TYPE.ERROR;
+
+        this.buildNotification();
+    }
+
     buildInfoNotificationOnBottomLeft() {
+        this.location = NotificationLocation.NOTIFICATION_LOCATION.BOTTOM_LEFT;
+        this.type = NotificationType.NOTIFICATION_TYPE.INFO;
+
+        this.buildNotification();
+    }
+
+    buildInfoNotificationOnBottomRight() {
         this.location = NotificationLocation.NOTIFICATION_LOCATION.BOTTOM_LEFT;
         this.type = NotificationType.NOTIFICATION_TYPE.INFO;
 
